f86758fe7646b92bc5c0755357d5dc57342181eb
Misirter E <ehoughton@deakin.edu.au>
2023-09-23 22:58:56 +1000

    Added router functionality

---
+++ portfolioapp/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h3>HTML/CSS/JavaScript Project</h3>
+    <ul>
+      <li><a href="https://github.com/EvanHoughton/SIT1201.2P" target="_blank" rel="noopener">Basic Website</a></li>
+    </ul>
+    <h3>Vue Projects</h3>
+    <ul>
+      <li><a href="https://github.com/EvanHoughton/todoapp" target="_blank" rel="noopener">Todo List</a></li>
+      <li><a href="https://github.com/EvanHoughton/todoapp/tree/master" target="_blank" rel="noopener">Tic-Tac-Toe</a></li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ portfolioapp/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <p>I am an enthusiastic Information Technology Specialist with computer and Microsoft Office-suite skills and 2 years of experience in technology industries.</p>
+    <p>I have knowledge of various programming languages (HTML, CSS, Javascript, Python, C#, etc.), website design (Zoho, Wordpress), cryptography, mathematical skills, and defect elimination.</p>
+    <p>I am a developer who can perform under pressure to meet deadlines and objectives.</p>
+    <p>I have excellent problem solving and organizational skills and a strong work ethic.</p>
+  </main>
+</template>
